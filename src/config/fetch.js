3ecaf85c13d2c96886c53c768836beab170bf11c
sunzongbin <SerapSun@163.com>
2018-07-23 19:39:20 +0800

    代码注释

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -1,77 +1,94 @@
-import { baseUrl } from './env'
+import {baseUrl} from './env'
 
-export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
-	type = type.toUpperCase();
-	url = baseUrl + url;
+export default async (url = '', data = {}, type = 'GET', method = 'fetch') => {
+    //将请求的方法设置为大写
+    type = type.toUpperCase();
 
-	if (type == 'GET') {
-		let dataStr = ''; //数据拼接字符串
-		Object.keys(data).forEach(key => {
-			dataStr += key + '=' + data[key] + '&';
-		})
+    //设置请求的url
+    url = baseUrl + url;
 
-		if (dataStr !== '') {
-			dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
-			url = url + '?' + dataStr;
-		}
-	}
 
-	if (window.fetch && method == 'fetch') {
-		let requestConfig = {
-			credentials: 'include',
-			method: type,
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			mode: "cors",
-			cache: "force-cache"
-		}
+    //get方式请求
+    if (type == 'GET') {
+        let dataStr = ''; //数据拼接字符串
+        Object.keys(data).forEach(key => {
+            dataStr += key + '=' + data[key] + '&';
+        })
 
-		if (type == 'POST') {
-			Object.defineProperty(requestConfig, 'body', {
-				value: JSON.stringify(data)
-			})
-		}
-		
-		try {
-			const response = await fetch(url, requestConfig);
-			const responseJson = await response.json();
-			return responseJson
-		} catch (error) {
-			throw new Error(error)
-		}
-	} else {
-		return new Promise((resolve, reject) => {
-			let requestObj;
-			if (window.XMLHttpRequest) {
-				requestObj = new XMLHttpRequest();
-			} else {
-				requestObj = new ActiveXObject;
-			}
+        if (dataStr !== '') {
+            dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
+            url = url + '?' + dataStr;
+        }
+    }
 
-			let sendData = '';
-			if (type == 'POST') {
-				sendData = JSON.stringify(data);
-			}
+    //其他请求方法
+    if (window.fetch && method == 'fetch') {
 
-			requestObj.open(type, url, true);
-			requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			requestObj.send(sendData);
+        //请求配置
+        let requestConfig = {
+            credentials: 'include',
+            method: type,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            mode: "cors",
+            cache: "force-cache"
+        }
 
-			requestObj.onreadystatechange = () => {
-				if (requestObj.readyState == 4) {
-					if (requestObj.status == 200) {
-						let obj = requestObj.response
-						if (typeof obj !== 'object') {
-							obj = JSON.parse(obj);
-						}
-						resolve(obj)
-					} else {
-						reject(requestObj)
-					}
-				}
-			}
-		})
-	}
+        //如果请求类型为post，修改config对象的body属性的值
+        if (type == 'POST') {
+            Object.defineProperty(requestConfig, 'body', {
+                value: JSON.stringify(data)
+            })
+        }
+
+        try {
+            //同步等待响应结果
+            const response = await fetch(url, requestConfig);
+
+            //同步等待将结果转成json格式
+            const responseJson = await response.json();
+            return responseJson
+        } catch (error) {
+            throw new Error(error)
+        }
+    } else {
+
+        //如果不支持fetch就使用XMLHttpRequest的方式请求
+        return new Promise((resolve, reject) => {
+            let requestObj;
+            if (window.XMLHttpRequest) {
+                requestObj = new XMLHttpRequest();
+            } else {
+                requestObj = new ActiveXObject;
+            }
+
+            let sendData = '';
+            if (type == 'POST') {
+                sendData = JSON.stringify(data);
+            }
+
+            //状态变更处理函数
+            requestObj.onreadystatechange = () => {
+                //http响应已经完全接受
+                if (requestObj.readyState == 4) {
+                    //响应状态为200时
+                    if (requestObj.status == 200) {
+                        let obj = requestObj.response
+                        if (typeof obj !== 'object') {
+                            obj = JSON.parse(obj);
+                        }
+                        resolve(obj)
+                    } else {
+                        reject(requestObj)
+                    }
+                }
+            };
+
+            requestObj.open(type, url, true);
+            requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            requestObj.send(sendData);
+        })
+    }
 }
--- src/page/userList.vue
+++ src/page/userList.vue
@@ -2,6 +2,22 @@
     <div class="fillcontain">
         <head-top></head-top>
         <div class="table_container">
+            <el-form :inline="true" :model="formInline" class="demo-form-inline">
+                <el-form-item label="审批人">
+                    <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域">
+                    <el-select v-model="formInline.region" placeholder="活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">查询</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="table_container">
             <el-table
                 :data="tableData"
                 highlight-current-row
@@ -67,6 +83,10 @@
                 limit: 20,
                 count: 0,
                 currentPage: 1,
+                formInline:{
+                    user:"zhangsan",
+                    region:"nihao"
+                }
             }
         },
     	components: {
